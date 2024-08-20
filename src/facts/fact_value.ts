45c5a6d90b8a08eb682894164888bf21d4e302ec
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2024-08-19 22:09:37 -0700

    add fact_value (convenience) getter helpers

--- src/facts/fact_value.ts
+++ src/facts/fact_value.ts
@@ -1,4 +1,76 @@
 /**
+ * getFactValueValue returns the value of a fact value.
+ */
+export function getFactValueValue(value: FactValue): unknown | null {
+  if (!isFactValueValue(value)) {
+    return null;
+  }
+
+  return value["@value"];
+}
+
+/**
+ * getFactValueID returns the ID of a fact value.
+ */
+export function getFactValueID(value: FactValue): string | null {
+  if (!isFactValueID(value)) {
+    return null;
+  }
+
+  return value["@id"];
+}
+
+/**
+ * getFactValueList returns the list of fact values.
+ */
+export function getFactValueList(value: FactValue): FactValue[] | null {
+  if (!isFactValueList(value)) {
+    return null;
+  }
+
+  return value["@list"];
+}
+
+/**
+ * getFactValueSet returns the set of fact values.
+ */
+export function getFactValueSet(value: FactValue): FactValue[] | null {
+  if (!isFactValueSet(value)) {
+    return null;
+  }
+
+  return value["@set"];
+}
+
+/**
+ * isFactValueValue returns true if the fact value is a value.
+ */
+export function isFactValueValue(value: FactValue): value is FactValueValue {
+  return "@value" in value;
+}
+
+/**
+ * isFactValueID returns true if the fact value is an ID.
+ */
+export function isFactValueID(value: FactValue): value is FactValueID {
+  return "@id" in value;
+}
+
+/**
+ * isFactValueList returns true if the fact value is a list.
+ */
+export function isFactValueList(value: FactValue): value is FactValueList {
+  return "@list" in value;
+}
+
+/**
+ * isFactValueSet returns true if the fact value is a set.
+ */
+export function isFactValueSet(value: FactValue): value is FactValueSet {
+  return "@set" in value;
+}
+
+/**
  * FactValue is a type that represents the value of a fact.
  */
 export type FactValue =
